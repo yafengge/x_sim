774fc8adffa77300d5979fb34dcc58219c3b644f
steven <yafenge126@126.com>
2025-12-10 14:09:34 +0800

    Inline clock/mem creation in build_all and pass to build_cube

--- sim/sim_top.cpp
+++ sim/sim_top.cpp
@@ -5,26 +5,19 @@ SimTop::SimTop(const SystolicConfig& cfg)
   build_all();
 }
 
-std::shared_ptr<Clock> SimTop::build_clock() {
-  return std::make_shared<Clock>();
-}
-
-std::shared_ptr<Mem> SimTop::build_mem(const std::shared_ptr<Clock>& clk) {
-  int max_outstanding = config_.max_outstanding > 0 ? config_.max_outstanding : 0;
-  int issue_bw = config_.bandwidth;
-  int complete_bw = config_.bandwidth;
-  return std::make_shared<Mem>(64, config_.memory_latency, issue_bw, complete_bw, max_outstanding, clk);
-}
-
-Cube& SimTop::build_cube() {
+Cube& SimTop::build_cube(const std::shared_ptr<Clock>& clk,
+                         const std::shared_ptr<Mem>& mem) {
   if (!cube_) {
-    auto clk = build_clock();
-    auto mem = build_mem(clk);
     cube_ = std::make_unique<Cube>(config_, clk, mem);
   }
   return *cube_;
 }
 
 void SimTop::build_all() {
-  build_cube();
+  auto clk = std::make_shared<Clock>();
+  int max_outstanding = config_.max_outstanding > 0 ? config_.max_outstanding : 0;
+  int issue_bw = config_.bandwidth;
+  int complete_bw = config_.bandwidth;
+  auto mem = std::make_shared<Mem>(64, config_.memory_latency, issue_bw, complete_bw, max_outstanding, clk);
+  build_cube(clk, mem);
 }
--- sim/sim_top.h
+++ sim/sim_top.h
@@ -12,10 +12,9 @@ class SimTop {
 public:
     explicit SimTop(const SystolicConfig& cfg);
 
-    // 分步构建接口（使用临时变量，统一在 build_cube 调用）
-    std::shared_ptr<Clock> build_clock();
-    std::shared_ptr<Mem> build_mem(const std::shared_ptr<Clock>& clk);
-    Cube& build_cube();
+    // 分步构建接口（build_all 创建 clock/mem 后传给 build_cube）
+    Cube& build_cube(const std::shared_ptr<Clock>& clk,
+                     const std::shared_ptr<Mem>& mem);
     void build_all();
 
     // 访问器
